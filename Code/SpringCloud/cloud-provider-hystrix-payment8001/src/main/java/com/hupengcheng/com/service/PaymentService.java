3d6c4f091eb67a5a2366e5d4d9c4786f72d75703
StudyRecording <canmengduanqing@gmail.com>
2020-08-21 21:25:45 +0800

    hystrix study init

---
+++ Code/SpringCloud/cloud-provider-hystrix-payment8001/src/main/java/com/hupengcheng/com/service/PaymentService.java
@@ -0,0 +1,25 @@
+package com.hupengcheng.com.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class PaymentService {
+
+    public String paymentInfo_OK(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + "  paymentInfo_OK,id:  " + id + "\t" + "哈哈～";
+    }
+
+    public String paymentInfo_TimeOut(Integer id) {
+
+        int timeNumber = 3;
+        try {
+            TimeUnit.SECONDS.sleep(timeNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "线程池：" + Thread.currentThread().getName() + "  paymentInfo_TimeOut,id:  " + id + "\t" + "呜呜～";
+    }
+}
