8c32e6917a4b9ea34d914c6b88f641cfc2546153
StudyRecording <canmengduanqing@gmail.com>
2020-08-27 21:56:00 +0800

    GateWay网关配置

---
+++ Code/SpringCloud/cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,10 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+        // 2020-08-27T21:25:24.310683+08:00[Asia/Shanghai]
+    }
+}
